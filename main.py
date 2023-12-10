8ff67d03418e97a023e73a91ee5385795effa0ef
Jessica Chen <jessicachen@Jessicas-MacBook-Pro-2.local>
2023-12-10 15:00:43 -0500

    plots for stability rates done

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from marketplace import Marketplace
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
 
 
 if __name__ == '__main__':
@@ -8,5 +11,74 @@ if __name__ == '__main__':
     # epsilon is grouped_epsilon if grouped, std if stochastic
     # if using BASELINE, epsilon is not used
     # if GROUPED, epsilon must be > 0
-    market = Marketplace(15, 4, 1, 0.5, "WEIGHTED_RANK", "GROUPED", 0.05)
-    print(market.stable)
+
+    NUM_AGENTS = 20
+    NUM_ROUNDS = 50
+    DURATION_MEAN = 6
+    DURATION_STD = 2
+    PRICE_STD = 1
+
+    # stability rate vs. epsilon
+    stability_rates = []
+    avg_revenues = [] # total system revenue per round
+    epsilons = np.arange(0.005, 0.5, 0.005)
+    for epsilon in epsilons:
+        stable_count = 0
+        total_revenue = 0
+        for round in range(NUM_ROUNDS):
+            market = Marketplace(NUM_AGENTS, DURATION_MEAN, DURATION_STD, PRICE_STD, "WEIGHTED_RANK", "GROUPED", epsilon)
+            total_revenue = sum([lender.price for lender in market.lenders])
+            if market.is_stable():
+                stable_count += 1
+        print(f"Stability rate for epsilon = {epsilon}: {stable_count / NUM_ROUNDS}")
+        print(f"Avg revenue for epsilon = {epsilon}: {total_revenue / NUM_ROUNDS}")
+        stability_rates.append(stable_count / NUM_ROUNDS)
+        avg_revenues.append(total_revenue / NUM_ROUNDS)
+
+    # LOWESS (Locally Weighted Scatterplot Smoothing)
+    lowess = sm.nonparametric.lowess(stability_rates, epsilons, frac=0.3)
+
+    # Scatter plot
+    plt.scatter(epsilons, stability_rates, color='blue', label='Data Points')
+
+    # Trendline
+    plt.plot(lowess[:, 0], lowess[:, 1], color='red', label='LOWESS Trendline')
+
+
+    plt.plot(epsilons, stability_rates, marker='o')
+    plt.xlabel('Epsilon (Group Size)')
+    plt.ylabel('Stability Rate')
+    plt.title('Grouped Indifference: Stability Rate vs Epsilon in Marketplace Simulations')
+    plt.grid(True)
+    plt.show()
+
+    # # stability rate vs. std 
+    # stability_rates = []
+    # stds = np.arange(0, 0.1, 0.001)
+    # for std in stds:
+    #     stable_count = 0
+    #     for round in range(NUM_ROUNDS):
+    #         market = Marketplace(NUM_AGENTS, DURATION_MEAN, DURATION_STD, PRICE_STD, "WEIGHTED_RANK", "STOCHASTIC", std)
+    #         if market.is_stable():
+    #             stable_count += 1
+    #     print(f"Stability rate for std = {std}: {stable_count / NUM_ROUNDS}")
+    #     stability_rates.append(stable_count / NUM_ROUNDS)
+
+    # # LOWESS (Locally Weighted Scatterplot Smoothing)
+    # lowess = sm.nonparametric.lowess(stability_rates, stds, frac=0.3)
+
+    # # Scatter plot
+    # plt.scatter(stds, stability_rates, color='blue', label='Data Points')
+
+    # # Trendline
+    # plt.plot(lowess[:, 0], lowess[:, 1], color='red', label='LOWESS Trendline')
+
+    # plt.plot(stds, stability_rates, marker='o')
+    # plt.xlabel('Standard Dev of True from Reported Preference Weight')
+    # plt.ylabel('Stability Rate')
+    # plt.title('Stochastic Indifference: Stability Rate vs Std in Marketplace Simulations')
+    # plt.grid(True)
+    # plt.show()
+
+    
+    
--- marketplace.py
+++ marketplace.py
@@ -7,17 +7,17 @@ class Marketplace():
         self.duration_std = duration_std
         self.price_std = price_std
 
-        print(f"Generating {num_agents} renters and lenders...")
+        # print(f"Generating {num_agents} renters and lenders...")
         self.renters = [Renter(i, duration_mean, duration_std, price_std) for i in range(self.num_agents)]
         self.lenders = [Lender(i, duration_mean, duration_std, price_std) for i in range(self.num_agents)]
 
-        print(f"Calculating reported pref orders using {report_algo} and true pref orders using {true_algo}...")
+        # print(f"Calculating reported pref orders using {report_algo} and true pref orders using {true_algo}...")
         self.generate_pref_orders(report_algo, true_algo, epsilon)
 
-        print(f"Performing Gale-Shapley matching...")
+        # print(f"Performing Gale-Shapley matching...")
         self.matchings = self.match()
 
-        print(f"Checking for stability...")
+        # print(f"Checking for stability...")
         self.stable = self.is_stable()
 
     def generate_pref_orders(self, report_algo, true_algo, epsilon):
@@ -119,8 +119,8 @@ class Marketplace():
             for renter in self.renters
         }
         
-        print("Renter preferences:",[[[lender.id for lender in lenders] for lenders in renter.reported_pref_order] for renter in self.renters])
-        print("Lender preferences:", [[[renter.id for renter in renters] for renters in lender.reported_pref_order] for lender in self.lenders])
+        # print("Renter preferences:",[[[lender.id for lender in lenders] for lenders in renter.reported_pref_order] for renter in self.renters])
+        # print("Lender preferences:", [[[renter.id for renter in renters] for renters in lender.reported_pref_order] for lender in self.lenders])
         
 
         # initialize set of unmatched renters
@@ -129,7 +129,7 @@ class Marketplace():
         while free_renters: # while there exists unmatched renters
             # select a renter
             renter = free_renters.pop()
-            print(f"renter {renter.id}")
+            # print(f"renter {renter.id}")
             
             for preferred_group in renter.reported_pref_order:
                 for preferred_lender in preferred_group:
@@ -138,7 +138,7 @@ class Marketplace():
                         preferred_lender.match = renter
                         renter.match = preferred_lender
                         # print("matched empty")
-                        print(f"Matched empty: Renter {renter.id} matched with lender {preferred_lender.id}")
+                        # print(f"Matched empty: Renter {renter.id} matched with lender {preferred_lender.id}")
                         break
                     # if lender has a prior match, check higher priority
                     else:
@@ -147,85 +147,85 @@ class Marketplace():
 
                         if new_match_rank < current_match_rank:
                             # If the lender prefers the new renter, make the switch
-                            print(f"Preferred lender {preferred_lender.id} was last matched with renter {preferred_lender.match.id}")
+                            # print(f"Preferred lender {preferred_lender.id} was last matched with renter {preferred_lender.match.id}")
                             preferred_lender.match.match = None
                             free_renters.add(preferred_lender.match)
                             preferred_lender.match = renter
                             renter.match = preferred_lender
                             # print("matched non-empty")
-                            print(f"Matched non-empty: Renter {renter.id} matched with lender {preferred_lender.id}")
+                            # print(f"Matched non-empty: Renter {renter.id} matched with lender {preferred_lender.id}")
                             break
                 if renter.match:
                     break 
         
         matchings = [(renter.id, renter.match.id) for renter in self.renters]
 
-        print(f"(renter,lender) matches: {matchings}")
+        # print(f"(renter,lender) matches: {matchings}")
         return matchings
     
     def is_stable(self):
         # Check each lender
         for lender in self.lenders:
-            print(f"BIG LOOP: Lender {lender.id}")
+            # print(f"BIG LOOP: Lender {lender.id}")
             lender_current_match_group_index = None
             for i, preferred_group in enumerate(lender.true_pref_order):
                 if lender.match in preferred_group:
                     lender_current_match_group_index = i
-                    print(f"Lender {lender.id} is matched with renter {lender.match.id}, which is in true pref group {lender_current_match_group_index}")
+                    # print(f"Lender {lender.id} is matched with renter {lender.match.id}, which is in true pref group {lender_current_match_group_index}")
                     break
 
             if lender_current_match_group_index is None:
                 # when would this happen?
-                print(f"Lender {lender.id}'s match is not found in any of its true pref groups.")
+                # print(f"Lender {lender.id}'s match is not found in any of its true pref groups.")
                 continue  # If the current match is not found in any group
 
             # checking all the "more preferred" groups for potential unstable pairs
             for preferred_group in lender.true_pref_order[:lender_current_match_group_index]:
                 for preferred_renter in preferred_group:
-                    print(f"Checking preferred renter {preferred_renter.id}")
+                    # print(f"Checking preferred renter {preferred_renter.id}")
                     # Check if the preferred renter also prefers this lender over their current match
                     renter_current_match_group_index = None
                     for j, renter_preferred_group in enumerate(preferred_renter.true_pref_order):
                         if preferred_renter.match in renter_preferred_group:
                             renter_current_match_group_index = j
-                            print(f"Renter {preferred_renter.id} is matched with {preferred_renter.match.id} in true pref group {renter_current_match_group_index}")
+                            # print(f"Renter {preferred_renter.id} is matched with {preferred_renter.match.id} in true pref group {renter_current_match_group_index}")
                             break
 
                     # does this change for indifference?
                     # can be in same group (index + 1)
                     if any(lender in group for group in preferred_renter.true_pref_order[:renter_current_match_group_index + 1]):
-                        print("Found unstable pair.")
+                        # print("Found unstable pair.")
                         return False  # Found an unstable pair
 
         # Check each renter
         for renter in self.renters:
-            print(f"BIG LOOP: Renter {renter.id}")
+            # print(f"BIG LOOP: Renter {renter.id}")
             renter_current_match_group_index = None
             for i, preferred_group in enumerate(renter.true_pref_order):
                 if renter.match in preferred_group:
                     renter_current_match_group_index = i
-                    print(f"Renter {renter.id} is matched with lender {renter.match.id}, which is in true pref group {renter_current_match_group_index}")
+                    # print(f"Renter {renter.id} is matched with lender {renter.match.id}, which is in true pref group {renter_current_match_group_index}")
                     break
 
             if renter_current_match_group_index is None:
-                print(f"Renter {renter.id}'s match is not found in any of its true pref groups.")
+                # print(f"Renter {renter.id}'s match is not found in any of its true pref groups.")
                 continue  # If the current match is not found in any group
 
             # checking all the "more preferred" groups for potential unstable pairs
             for preferred_group in renter.true_pref_order[:renter_current_match_group_index]:
                 for preferred_lender in preferred_group:
-                    print(f"Checking preferred lender {preferred_lender.id}")
+                    # print(f"Checking preferred lender {preferred_lender.id}")
                     # Check if the preferred renter also prefers this lender over their current match
                     lender_current_match_group_index = None
                     for j, lender_preferred_group in enumerate(preferred_lender.true_pref_order):
                         if preferred_lender.match in lender_preferred_group:
                             lender_current_match_group_index = j
-                            print(f"Lender {preferred_lender.id} is matched with {preferred_lender.match.id} in true pref group {lender_current_match_group_index}")
+                            # print(f"Lender {preferred_lender.id} is matched with {preferred_lender.match.id} in true pref group {lender_current_match_group_index}")
                             break
                     
                     # can be in same group
                     if any(renter in group for group in preferred_lender.true_pref_order[:lender_current_match_group_index+1]):
-                        print("Found unstable pair.")
+                        # print("Found unstable pair.")
                         return False  # Found an unstable pair
 
         return True
